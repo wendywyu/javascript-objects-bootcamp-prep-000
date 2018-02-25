86f8c03b9e0291a135d4dc57c624541da81b16f4
wendy yu <yu.wendyw@gmail.com>
2018-02-25 20:30:31 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist = { Kanye: 'Gold Digger'};
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  playlist[artistName] = 'songTitle'
+  playlist[artistName] = songTitle
   return playlist
 }
 
